477366a9c574e3603263b21f1a224f5373c4ec4e
amandine-malo <amandine.malo@ens.uvsq.fr>
2019-11-17 15:17:47 +0100

    projet

---
+++ Pompe.h
@@ -0,0 +1,14 @@
+#ifndef _POMPE_H
+#define _POMPE_H
+
+#include <iostring>
+
+class Pompe {
+	private :
+		
+	public :
+			Pompe();
+			~Pompe();
+}
+
+#endif
---
+++ Simulateur.h
@@ -0,0 +1,4 @@
+#ifndef _SIMULATEUR_H
+#define _SIMULATEUR_H
+
+#endif
